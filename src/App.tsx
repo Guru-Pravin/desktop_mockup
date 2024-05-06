115ac3d65f5d828c9bf27169b028cca362737141
Aakash Srinivasan <86234579+Aakash-Srinivasan@users.noreply.github.com>
2024-05-06 15:57:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <TabButtons numberOfApprovals={89} />
+      <FilterComponent />
       <Memos />
       <Approvaldetail />
     </>
