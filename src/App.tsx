a3b15ab82152d69a544df132643bb6ac3b6999ef
Aakash <Aakash.S@skillassure.com>
2024-05-06 16:03:09 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Approvaldetail from './components/Approvaldetails.tsx';
 import Memos from "./components/Memos.tsx";
 import Navbar from './components/Navbar.tsx';
+import TabButtons from './components/TabButtons.tsx';
+import FilterComponent from './components/FilterComponent.tsx';
 
 export default function App() {
   return (
